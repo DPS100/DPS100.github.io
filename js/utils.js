f9dcced877b9a7381f4e3f12240d729f0fec9fce
dgpenastein <dgpenastein@wpi.edu>
2024-12-18 00:46:26 -0500

    Update templating

--- js/utils.js
+++ js/utils.js
@@ -24,15 +24,36 @@ function populateProject(json, id) {
             }
         }
     }
-    var image_div = clone.getElementsByClassName('images')[0]
-    for(image of json.metadata.images) {
-        image_div.style.display = "block"
-        image_template = image_div.getElementsByClassName('image-template')[0].cloneNode(true);
-        image_template.class = ""
-        image_template.style.display = "block"
-        image_template.src=image
-        image_div.appendChild(image_template);
+
+    populateDiv(clone, json, 'images', 'image-template', 'images')
+    populateDiv(clone, json, 'videos', 'video-template', 'videos')
+    populateDiv(clone, json, 'media', 'media-template', 'media')
+    
+    var link_div = clone.getElementsByClassName('links')[0]
+    for(link of json.metadata.links) {
+        link_div.style.display = "block"
+        link_template = link_div.getElementsByClassName('link-template')[0].cloneNode(true);
+        link_a_template = link_template.getElementsByClassName('link-a-template')[0];
+        link_a_template.class = "";
+        link_a_template.href = link.link
+        link_a_template.innerHTML = link.link
+        link_template.class = ""
+        link_template.style.display = "block"
+        link_template.innerHTML = link.title + ": " + link_template.innerHTML
+        link_div.appendChild(link_template);
     }
 
     element.parentElement.appendChild(clone)
+}
+
+function populateDiv(parent, json, div_name, template_name, metadata_target) {
+    var div = parent.getElementsByClassName(div_name)[0]
+    for(content of json.metadata[metadata_target]) {
+        div.style.display = "block"
+        template = div.getElementsByClassName(template_name)[0].cloneNode(true);
+        template.class = ""
+        template.style.display = "block"
+        template.src=content
+        div.appendChild(template);
+    }
 }
